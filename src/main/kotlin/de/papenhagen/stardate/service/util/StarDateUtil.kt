dcc20efbd0616b6e243b404e504dddeaeb3643bb
Jens Papenhagen <jens.papenhagen@web.de>
2025-09-14 10:49:47 +0200

    add util for calucation of the stardate

---
+++ src/main/kotlin/de/papenhagen/stardate/service/util/StarDateUtil.kt
@@ -0,0 +1,27 @@
+package de.papenhagen.stardate.service.util
+
+import java.time.LocalDateTime
+
+open class StarDateUtil {
+    companion object {
+        private const val TOS_BASE_YEAR: Int = 2265 //Year TOS starts
+        private const val UNITS_PER_YEAR: Double = 1000.0
+
+        fun calc(dateTime: LocalDateTime): String {
+            val year = dateTime.year
+            val dayOfYear = dateTime.dayOfYear
+            val hour = dateTime.hour
+            val minute = dateTime.minute
+            val second = dateTime.second
+
+            val secondsInDay = 86400.0
+            val secondsToday = (hour * 3600 + minute * 60 + second)
+            val fractionalDay = secondsToday / secondsInDay
+            val fractionalYear: Double = (dayOfYear - 1 + fractionalDay) / 365.25
+
+            val stardate: Double = (year - TOS_BASE_YEAR + fractionalYear) * UNITS_PER_YEAR + 1000.0
+
+            return String.format("%.1f", stardate)
+        }
+    }
+}
---
+++ src/test/kotlin/de/papenhagen/stardate/service/util/StarDateUtilTest.kt
@@ -0,0 +1,60 @@
+package de.papenhagen.stardate.service.util
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.time.Clock
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.*
+
+
+class StarDateUtilTest {
+    private var clock: Clock? = null
+    private lateinit var storedLocale: Locale
+
+    @BeforeEach
+    fun setUp() {
+        //set local for period/comma rules
+        storedLocale = Locale.GERMAN
+
+        val instantExpected = "2025-09-14T10:17:25Z"
+        clock = Clock.fixed(Instant.parse(instantExpected), ZoneId.of("UTC"))
+    }
+
+    @AfterEach
+    fun teardown() {
+        Locale.setDefault(storedLocale)
+    }
+
+
+    @Test
+    fun testingStarDateGeneration() {
+        //given
+        val now = LocalDateTime.now(clock)
+
+        //when
+        val calcStarDate = StarDateUtil.calc(now)
+
+        //then
+        assertThat(calcStarDate).isNotBlank
+        assertThat(calcStarDate).isEqualTo("-238297,9")
+    }
+
+    @Test
+    fun originalSeriesStart() {
+        //given
+        val tosDate = LocalDateTime.of(2266, 1, 1, 0, 0)
+
+        //when
+        val calcStarDate = StarDateUtil.calc(tosDate)
+
+        //then
+        assertThat(calcStarDate).isNotBlank
+        assertThat(calcStarDate).isEqualTo("2000,0")
+    }
+
+
+}
