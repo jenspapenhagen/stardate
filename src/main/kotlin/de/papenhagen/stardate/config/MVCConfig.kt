98f5723075bdf3e2a4f2ff0ed10efd7367bb14e3
Jens Papenhagen <jens.papenhagen@web.de>
2025-09-15 21:27:19 +0200

    Update MVCConfig.kt

--- src/main/kotlin/de/papenhagen/stardate/config/MVCConfig.kt
+++ src/main/kotlin/de/papenhagen/stardate/config/MVCConfig.kt
@@ -12,6 +12,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class MVCConfig : WebMvcConfigurer {
+    
+    /**
+    * This is the default jackson object mapper from Spring. 
+    * Needed for having a constant object mapper
+    * or Spring MVC will load a jackson object mapper out of the class path
+    * that can be not the right one.
+    *
+    * @return a Jackson2ObjectMapperBuilder instance
+    */
+    @Bean
+    fun jackson2ObjectMapperBuilder(): Jackson2ObjectMapperBuilder =
+    Jackson2ObjectMapperBuilder()
+        .featuresToEnable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES)
+        .featuresToEnable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+        .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        .modulesToInstall(
+            JsonNullableModule(),
+            ParameterNamesModule(),
+            JavaTimeModule())
+        .dateFormat(SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX"))
+        
     @Bean
     fun objectMapper(): ObjectMapper {
         val kotlinModule =
