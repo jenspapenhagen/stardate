8de6561a61b82a458d7970b9355c5dc7b4a9feda
Jens Papenhagen <jens.papenhagen@web.de>
2025-09-16 21:20:39 +0200

    ktlint and ad missing imports

--- src/main/kotlin/de/papenhagen/stardate/config/MVCConfig.kt
+++ src/main/kotlin/de/papenhagen/stardate/config/MVCConfig.kt
@@ -6,33 +6,32 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinFeature
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.text.SimpleDateFormat
 
 @Configuration
 class MVCConfig : WebMvcConfigurer {
-    
     /**
-    * This is the default jackson object mapper from Spring. 
-    * Needed for having a constant object mapper
-    * or Spring MVC will load a jackson object mapper out of the class path
-    * that can be not the right one.
-    *
-    * @return a Jackson2ObjectMapperBuilder instance
-    */
+     * This is the default jackson object mapper from Spring.
+     * Needed for having a constant object mapper
+     * or Spring MVC will load a jackson object mapper out of the class path
+     * that can be not the right one.
+     *
+     * @return a Jackson2ObjectMapperBuilder instance
+     */
     @Bean
     fun jackson2ObjectMapperBuilder(): Jackson2ObjectMapperBuilder =
-    Jackson2ObjectMapperBuilder()
-        .featuresToEnable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES)
-        .featuresToEnable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-        .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-        .modulesToInstall(
-            JsonNullableModule(),
-            ParameterNamesModule(),
-            JavaTimeModule())
-        .dateFormat(SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX"))
-        
+        Jackson2ObjectMapperBuilder()
+            .featuresToEnable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES)
+            .featuresToEnable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .modulesToInstall(ParameterNamesModule(), JavaTimeModule())
+            .dateFormat(SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX"))
+
     @Bean
     fun objectMapper(): ObjectMapper {
         val kotlinModule =
